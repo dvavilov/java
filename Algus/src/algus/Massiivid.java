86a727116f295637d23f4df2d6a6153637b33118
if17 <if17@A406-27.INFORMAATIKA>
2018-02-01 10:41:25 +0200

    Projekti algus.Temperatuur.

--- Algus/src/algus/Massiivid.java
+++ Algus/src/algus/Massiivid.java
@@ -7,6 +7,17 @@ public class Massiivid {
         for(int i=0; i<pikkused.length; i++){
             System.out.println(pikkused[i]);
         }
-    }
+        //Arvuta pikkuste summa
+        int summa=0;
+        for(int i=0; i<pikkused.length; i++){
+            summa+=pikkused[i];
+            
+        }
     
+        System.out.println(summa);
+        summa=0;
+        for(int pikkus: pikkused){summa+=pikkus;}
+        System.out.println(summa);
+        System.out.println(Arrays.stream(pikkused).sum());
+    }
 }
