6c2dde26cf8d5be7a2c1bcd50885e53c0e7d0e0d
if17 <if17@A406-27.INFORMAATIKA>
2018-02-08 09:30:34 +0200

    Klassid

--- Klassid/src/klassid/Pall.java
+++ Klassid/src/klassid/Pall.java
@@ -4,6 +4,9 @@ package klassid;
 public class Pall {
     protected double x;
     protected double y;
+    //lisage pallile raadius
+    //Lisage kask kontrollimaks, kas kaks palli puutuvad kokku
+    
     public Pall(double x, double y){
         this.x=x;
         this.y=y;
@@ -11,6 +14,16 @@ public class Pall {
     }
     public double kaugustNullist(){
         return Math.sqrt(x*x+y*y);//kaivitage 
+    }
+    public double getX(){return x;}
+    public double getY(){return y;}
+    public double kaugustPallist(Pall teine){
+        double dx=getX()-teine.getX();
+        double dy=getY()-teine.getY();
+        return Math.sqrt(dx*dx+dy+dy);
+    }
+    public double R(){
+    return Math.sqrt(x)
     }
     @Override
     public String toString(){
--- Klassid/src/klassid/Proov.java
+++ Klassid/src/klassid/Proov.java
@@ -5,8 +5,9 @@ public class Proov {
         public static void main(String[] arg){
             Pall p1=new Pall(3,5);
             Pall p2=new Pall(4,6);
-            System.out.println(p1);
+            System.out.println(p1 + "Kaugus algkohast: "+p1.kaugustNullist());
             System.out.println(p2);
+            System.out.println();
         }
     
 }
