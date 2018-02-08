fd7ecdce52b784140abf6e48e6f7ea9fcda43039
if17 <if17@A406-27.INFORMAATIKA>
2018-02-08 09:12:05 +0200

    Piirid

--- Klassid/src/klassid/Pall.java
+++ Klassid/src/klassid/Pall.java
@@ -4,4 +4,13 @@ package klassid;
 public class Pall {
     protected double x;
     protected double y;
+    public Pall(double x, double y){
+        this.x=x;
+        this.y=y;
+    
+    }
+    @Override
+    public String toString(){
+        return "Pall kohal ("+x+","+y+")";
+    }
 }
---
+++ Klassid/src/klassid/Proov.java
@@ -0,0 +1,12 @@
+package klassid;
+
+
+public class Proov {
+        public static void main(String[] arg){
+            Pall p1=new Pall(3,5);
+            Pall p2=new Pall(4,6);
+            System.out.println(p1);
+            System.out.println(p2);
+        }
+    
+}
