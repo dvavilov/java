8c98e89f4aad8c93886f35e20a03426c16184606
if17 <if17@A406-27.INFORMAATIKA>
2018-02-08 10:55:46 +0200

    Klassid

--- Klassid/src/klassid/Proov2.java
+++ Klassid/src/klassid/Proov2.java
@@ -5,9 +5,21 @@ public class Proov2 {
      pallid[0]=new Pall(3, 5);
      pallid[1]=new Pall(4, 6);
      pallid[2]=new Pall(-10, 8, 2);
+     int kaugeim=0;
      for(int i=0; i<pallid.length; i++){
         System.out.println(pallid[i]);
-        //Kuvage koordinaatide algustpunktist kÃµige kaugema palli andmed
+        //Kuvage koordinaatide algustpunktist kõige kaugema palli andmed
+        
+        if(pallid[i].kaugusNullist()>pallid[kaugeim].kaugusNullist()){
+            kaugeim=i;
+        }
+     
      }
+     System.out.println("Kaugeim: "+pallid[kaugeim]);
+     if(pallid[i].kaugusNullist()>pallid[kaugeim].kaugusNullist()){
+            kaugeim=i;
+            }
+     
+    System.out.println() 
   }    
 }
